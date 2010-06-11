0c98c832b197da88cd148cb4bee8c777e4433ba1
Alan Dipert <alan.dipert@gmail.com>
2010-06-11 19:11:53 -0400

    fixed on mac. thanks lee!

--- src/clj/org/dipert/swingrepl/main.clj
+++ src/clj/org/dipert/swingrepl/main.clj
@@ -17,27 +17,27 @@
       :title (str "Clojure " clj-version)
       :on-close JFrame/DISPOSE_ON_CLOSE})
 
-(defn- make-jframe
-  [{:keys [title width height on-close]}]
-  (doto (JFrame. title)
-    (.setSize width height)
-    (.setDefaultCloseOperation on-close)
-    (.setLocationRelativeTo nil)
-    (.setVisible true)))
-
-(defn- make-repl-jframe
+(defn make-repl-jframe
   "Displays a JFrame with JConsole and attached REPL."
   ([] (make-repl-jframe {}))
   ([optmap]
-     (let [jframe (make-jframe (merge default-opts optmap))]
-       (javax.swing.SwingUtilities/invokeAndWait
-	#(let [console (bsh.util.JConsole.)]
+     (let [options (merge default-opts optmap)
+	   {:keys [title width height on-close]} options
+	   jframe (doto (JFrame. title)
+		    (.setSize width height)
+		    (.setDefaultCloseOperation on-close)
+		    (.setLocationRelativeTo nil)
+		    (.setVisible true))]
+	(let [console (bsh.util.JConsole.)]
 	   (doto (.getContentPane jframe)
 	     (.setLayout (java.awt.BorderLayout.))
 	     (.add console))
+	   (doto jframe
+	     (.pack)
+	     (.setSize width height))
 	   (binding [*out* (java.io.OutputStreamWriter. (.getOut console))
 		     *in*  (clojure.lang.LineNumberingPushbackReader. (.getIn console))]
-	     (.start (Thread. (bound-fn [] (clojure.main/repl))))))))))
+	     (.start (Thread. (bound-fn [] (clojure.main/repl)))))))))
 
 (defn -main
   [& args]
