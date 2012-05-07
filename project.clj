4c1fbed4e659ba3bf06a05d3794ad1d3b7ac31ac
Ivan Kozik <ivan@ludios.org>
2012-05-07 04:35:10 +0000

    project.clj: Set minimum Clojure version to 1.3.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :java-fork "true"
   :javac-debug "true"
   :description "A Swing Clojure REPL using BeanShell's JConsole"
-  :dependencies [[org.clojure/clojure "1.3.0-alpha2"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[lein-javac "1.2.1-SNAPSHOT"]
                      [swank-clojure "1.3.0-SNAPSHOT"]]
   :hooks [leiningen.hooks.javac]
