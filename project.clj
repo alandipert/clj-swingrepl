e5aaabe9a4455cb564b399b6bb1c1a419cf93b90
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-10-24 16:53:00 +0100

    Revert "Dependency changes."
    This reverts commit 120632999a0d00145c6102f5e899f9793714e5df.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject swingrepl "1.4.1-SNAPSHOT"
   :source-paths ["src/clj"]
   :java-source-paths ["src/jvm"]
-  :javac-options     ["-target" "1.6" "-source" "1.6"]
   :description "A Swing Clojure REPL using BeanShell's JConsole"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]]
   :main org.dipert.swingrepl.main)
